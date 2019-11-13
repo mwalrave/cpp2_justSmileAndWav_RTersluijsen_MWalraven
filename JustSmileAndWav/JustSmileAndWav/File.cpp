9b20f7fe56d540b66071660cbcd5b00dcd224fdc
mwalrave <mwalrave@avans.nl>
2019-11-13 15:02:13 +0100

    Ez Decrypto
    Ez katga

--- JustSmileAndWav/JustSmileAndWav/File.cpp
+++ JustSmileAndWav/JustSmileAndWav/File.cpp
@@ -1,5 +1,6 @@
 #include "File.h"
 #include <fstream>
+#include <bitset>
 using namespace std;
 
 
@@ -9,10 +10,10 @@ File::File()
 }
 
 
-File::File(string pFileName, bool pLittleEndian) : fileName(pFileName), littleEndian( pLittleEndian)
+File::File(string pFileName, bool pLittleEndian) : fileName(pFileName), littleEndian(pLittleEndian)
 {
 	setFileInfo();
-	
+
 }
 
 void File::setFileInfo()
@@ -44,11 +45,11 @@ void File::setFileInfo()
 		int size = 0;
 		while (true) {
 			ifs.read(buffer.data(), 4);
-			ifs.read(chunkSize.data(), 4);	
-		
-		
+			ifs.read(chunkSize.data(), 4);
 
-		
+
+
+
 			size = toInt(chunkSize);
 			if (strstr(buffer.data(), "fmt")) {
 				std::vector<char> sampleSize(5);
@@ -56,38 +57,76 @@ void File::setFileInfo()
 				ifs.read(sampleSize.data(), 2);
 				bitsPerSample = toInt(sampleSize);
 			}
-			
-			else if (strstr(buffer.data(),"data")) {
-			
+
+			else if (strstr(buffer.data(), "data")) {
+
 				break;
 			}
 			else {
 
 				ifs.seekg(size, ifs.cur);
 			}
-			
+
 		}
 
 		cout << buffer.data() << " Data" << endl;
-		cout << size << " Data size" <<endl;
-		cout << bitsPerSample << " Bits per sample"<< endl;
-		int numberOfSamples = size / bitsPerSample;
-		cout << numberOfSamples << " NUMBAH " << endl;
-		std::vector< std::vector<char> > samples(numberOfSamples, vector<char>(bitsPerSample+4));
-		for(int i = 0; i < numberOfSamples; i++){
-			ifs.read(samples[i].data() , bitsPerSample);
-			cout << samples[i].data() << endl;
+		cout << size << " Data size" << endl;
+		cout << bitsPerSample << " Bits per sample" << endl;
+		int numberOfSamples = size / 2;
+		cout << numberOfSamples << " numberOfSamples " << endl;
+
+		std::vector<short> storage(size);
+		for (int i = 0; i < numberOfSamples; i++) {
+
+			ifs.read((char*)& storage[i], 2);
+
+
 		}
+
+
+		std::vector<bitset<8>> hiddenMessageBytes(numberOfSamples);
+		std::bitset<8> hiddenCharacterByte;
+		int j = 7;
+
+		for (int i = 0; i < numberOfSamples; i++) {
+
+			
+			int lsb = storage[i] & 1;
+			hiddenCharacterByte.set(j, lsb);
+
+			//cout << lsb << "";
+			if (j > 0) {
+				j--;
+			}
+			else { 
+				j = 7; 
+				if (hiddenCharacterByte.none()) {
+					break;
+				};
+				hiddenMessageBytes[i] = hiddenCharacterByte;
+				cout << (char)hiddenCharacterByte.to_ulong();
+				
+			}
+		}cout << " "<< endl;
 		ifs.close();
+
+
+	/*	std::string message = "";
+		for (int i = 0; i < hiddenMessageBytes.size(); i++) {
+			auto characterByte = hiddenMessageBytes.at(i);
+				cout << characterByte << endl;
+		}*/
+
+		//cout << endl << message << endl;
 
 	}
 
-	
+
 }
 
 void File::getFileInfo()
 {
-	
+
 }
 
 int File::toInt(std::vector<char> chunkSize)
